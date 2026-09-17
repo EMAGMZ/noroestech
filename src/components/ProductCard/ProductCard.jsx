021b28aa5c9053b284007565dac7a62344ce5eaf
Emanuel <manu.gomez.cap@gmail.com>
2026-09-17 19:08:08 -0300

    feat: navegar al detalle de producto al clickear una card

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,13 +1,14 @@
+import { Link } from 'react-router-dom'
 import styles from './ProductCard.module.css'
 
 function ProductCard({ producto }) {
   return (
-    <div className={styles.card}>
+    <Link to={`/producto/${producto.id}`} className={styles.card}>
       <img src={producto.img} alt={producto.name} />
       <h3>{producto.name}</h3>
       <p>{producto.description}</p>
       <p className={styles.price}>${producto.price.toLocaleString('es-AR')}</p>
-    </div>
+    </Link>
   )
 }
 
